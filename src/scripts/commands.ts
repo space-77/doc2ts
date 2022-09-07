2ac14f2a3cd196225c04ea08d58fb465b0671146
lirandong <lirandong@maxrocky.com>
2022-09-07 23:54:46 +0800

    121

--- src/scripts/commands.ts
+++ src/scripts/commands.ts
@@ -1,3 +1,6 @@
 export const GIT_VERSION = 'git --version'
 export const GET_REV_PARSE = 'git rev-parse HEAD'
-export const GET_CHECKOUT = 'git checkout'
+export const GET_CHECKOUT = 'git checkout '
+export const GIT_STATUS = 'git status'
+export const GIT_ADD = 'git add '
+export const GIT_COMMIT = 'git commit -m '
