903f6f3b7d70108abb96587fb6e95c96a191e629
lirandong <lirandong@maxrocky.com>
2022-09-27 00:19:29 +0800

    feat: update api file (doc2ts auto commmit).

--- services/module/tset/user.ts
+++ services/module/tset/user.ts
@@ -5,95 +5,87 @@ import type * as mT from '../../types/tset/user'
  * @description Operations about user
  */
 export default class User extends BaseClass {
-  
-/**
- * @name tsetCreateUser
- * @description Create user，This can only be done by the logged in user.
- */
-createUser: mT.CreateUser = body => {
-  const url = '/user'
-  const config = { '/user',body: body, method: 'post' }
-  return this.request(config)
-}
+  /**
+   * @name tsetCreateUser
+   * @description Create user，This can only be done by the logged in user.
+   */
+  createUser: mT.CreateUser = body => {
+    const url = '/user'
+    const config = { url, body: body, method: 'post' }
+    return this.request(config)
+  }
 
+  /**
+   * @name tsetLoginUser
+   * @description Logs user into the system
+   */
+  loginUser: mT.LoginUser = params => {
+    const url = `/user/login?${this.serialize({ params })}`
+    const config = { url, method: 'get' }
+    return this.request(config)
+  }
 
-/**
- * @name tsetLoginUser
- * @description Logs user into the system
- */
-loginUser: mT.LoginUser = params => {
-  const url = `/user/login?${this.serialize({params})}`
-  const config = { `/user/login?${this.serialize({params})}`, method: 'get' }
-  return this.request(config)
-}
+  /**
+   * @name tsetLogoutUser
+   * @description Logs out current logged in user session
+   */
+  logoutUser: mT.LogoutUser = () => {
+    const url = '/user/logout'
+    const config = { url, method: 'get' }
+    return this.request(config)
+  }
 
+  /**
+   * @name tsetGetUserByName
+   * @description Get user by user name
+   */
+  getUserByName: mT.GetUserByName = username => {
+    const url = `/user/${username}`
+    const config = { url, method: 'get' }
+    return this.request(config)
+  }
 
-/**
- * @name tsetLogoutUser
- * @description Logs out current logged in user session
- */
-logoutUser: mT.LogoutUser = () => {
-  const url = '/user/logout'
-  const config = { '/user/logout', method: 'get' }
-  return this.request(config)
-}
+  /**
+   * @name tsetUpdateUser
+   * @description Updated user，This can only be done by the logged in user.
+   */
+  updateUser: mT.UpdateUser = params => {
+    const { username, body: _body } = params
+    const body = { body: _body }
+    const url = `/user/${username}`
+    const config = { url, body, method: 'put' }
+    return this.request(config)
+  }
 
+  /**
+   * @name tsetDeleteUser
+   * @description Delete user，This can only be done by the logged in user.
+   */
+  deleteUser: mT.DeleteUser = username => {
+    const url = `/user/${username}`
+    const config = { url, method: 'delete' }
+    return this.request(config)
+  }
 
-/**
- * @name tsetGetUserByName
- * @description Get user by user name
- */
-getUserByName: mT.GetUserByName = username => {
-  const url = `/user/${username}`
-  const config = { `/user/${username}`, method: 'get' }
-  return this.request(config)
-}
+  /**
+   * @name tsetCreateUsersWithListInput
+   * @description Creates list of users with given input array
+   */
+  createUsersWithListInput: mT.CreateUsersWithListInput = body => {
+    const url = '/user/createWithList'
+    const config = { url, body: body, method: 'post' }
+    return this.request(config)
+  }
 
-
-/**
- * @name tsetUpdateUser
- * @description Updated user，This can only be done by the logged in user.
- */
-updateUser: mT.UpdateUser = params => {
-const { username,body:_body } = params
-const body = {body:_body}
-  const url = `/user/${username}`
-  const config = { `/user/${username}`, body, method: 'put' }
-  return this.request(config)
-}
-
-
-/**
- * @name tsetDeleteUser
- * @description Delete user，This can only be done by the logged in user.
- */
-deleteUser: mT.DeleteUser = username => {
-  const url = `/user/${username}`
-  const config = { `/user/${username}`, method: 'delete' }
-  return this.request(config)
-}
-
-
-/**
- * @name tsetCreateUsersWithListInput
- * @description Creates list of users with given input array
- */
-createUsersWithListInput: mT.CreateUsersWithListInput = body => {
-  const url = '/user/createWithList'
-  const config = { '/user/createWithList',body: body, method: 'post' }
-  return this.request(config)
-}
-
-
-/**
- * @name tsetCreateUsersWithArrayInput
- * @description Creates list of users with given input array
- */
-createUsersWithArrayInput: mT.CreateUsersWithArrayInput = body => {
-  const url = '/user/createWithArray'
-  const config = { '/user/createWithArray',body: body, method: 'post' }
-  return this.request(config)
-}
+  /**
+   * @name tsetCreateUsersWithArrayInput
+   * @description Creates list of users with given input array
+   */
+  createUsersWithArrayInput: mT.CreateUsersWithArrayInput = body => {
+    const url = '/user/createWithArray'
+    const config = { url, body: body, method: 'post' }
+    return this.request(config)
+  }
 }
 
 export const user = new User()
