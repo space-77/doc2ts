ab699129fba2764a97c2512b018e5956aace63bf
lirandong <lirandong@maxrocky.com>
2023-06-12 13:13:47 +0800

    chore: version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export type { Doc2TsConfig, ModelList } from './types/types'
 export type { IApiClient, DocReqConfig, Method, TData } from './types/client'
 export type { Surrounding } from './common/config'
+export { TranslateType } from 'doc-pre-data'
 // import Doc2Ts from './doc2TsCore'
 // import log from './utils/log'
 
